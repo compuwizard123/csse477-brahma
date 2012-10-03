d171a61c1bbcd888fe4977ded7140fbd32885833
Nick Crawford <crawfonw@rose-hulman.edu>
2012-10-03 19:36:07 -0400

    Add more PluginManagerTests

--- TestBrahma/tests/brahma/tests/PluginManagerTest.java
+++ TestBrahma/tests/brahma/tests/PluginManagerTest.java
@@ -2,6 +2,7 @@ package brahma.tests;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.nio.file.FileSystems;
 import java.nio.file.Path;
 
 import org.easymock.EasyMock;
@@ -14,10 +15,15 @@ import org.junit.Test;
 import brahma.host.Host;
 import brahma.host.PluginManager;
 
+//import org.apache.commons.io.FileUtils;
+
 public class PluginManagerTest {
 	
 	PluginManager p;
 	Host h;
+	
+	private final Path pluginPath = FileSystems.getDefault().getPath("plugins");
+	private final Path jarPath = FileSystems.getDefault().getPath("plugin.jar");
 
 	@Before
 	public void setUp() throws Exception {
@@ -46,6 +52,42 @@ public class PluginManagerTest {
 	public void testEmptyUnload() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException {
 		Path path = EasyMock.createMock(Path.class);
 		Method method = PluginManager.class.getDeclaredMethod("unloadBundle", Path.class);
+		method.setAccessible(true);
+		try {
+			method.invoke(p, path);
+		} catch (InvocationTargetException e) {
+			
+		}
+		Assert.assertEquals(p.getPathToPlugin().size(), 0);
+	}
+	
+	@Test
+	public void testSingleLoadBundle() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException {
+		FileUtils.copyFile(pluginPath, jarPath);
+		Path path = EasyMock.createMock(Path.class);
+		Method method = PluginManager.class.getDeclaredMethod("loadBundle", Path.class);
+		method.setAccessible(true);
+		try {
+			method.invoke(p, path);
+		} catch (InvocationTargetException e) {
+			
+		}
+		Assert.assertEquals(p.getPathToPlugin().size(), 1);
+	}
+	
+	@Test
+	public void testSingleUnloadBundle() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException {
+		FileUtils.copyFile(pluginPath, jarPath);
+		Path path = EasyMock.createMock(Path.class);
+		Method method = PluginManager.class.getDeclaredMethod("loadBundle", Path.class);
+		method.setAccessible(true);
+		try {
+			method.invoke(p, path);
+		} catch (InvocationTargetException e) {
+			
+		}
+		Assert.assertEquals(p.getPathToPlugin().size(), 1);
+		method = PluginManager.class.getDeclaredMethod("unloadBundle", Path.class);
 		method.setAccessible(true);
 		try {
 			method.invoke(p, path);
